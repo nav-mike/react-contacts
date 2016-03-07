832fd5c08a81ab0b3357d455dd7f25b890d55c0f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-03-07 14:12:36 +0300

    add contact row for index table

---
+++ assets/js/app/components/contact_row.js
@@ -0,0 +1,24 @@
+$(document).ready({
+  var ContactRow = React.createClass({
+    render: function(){
+      return(
+        <tr>
+          <td>{this.props.contact.get("id")}</td>
+          <td>{this.props.contact.get("name")}</td>
+          <td>{this.props.contact.get("value")}</td>
+          <td>{this.props.contact.get("title")}</td>
+          <td>
+            <button
+              className="btn btn-warning"
+              data-id={this.props.contact.get("id")}>Show</button>
+          </td>
+          <td>
+            <button
+              className="btn btn-danger"
+              data-id={this.props.contact.get("id")}>Delete</button>
+          </td>
+        </tr>
+      );
+    }
+  });
+});
